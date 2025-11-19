27c7441b7362c39ba1f69812ec08aa8dd76399aa
SebastianSPM <sebasperemald04@gmail.com>
2025-11-18 19:49:17 -0500

    add feat in the login

---
+++ src/pages/login.js
@@ -0,0 +1,16 @@
+export default function Login(){
+    return `
+        <div>
+            <h2>Login</h2>
+            <form>
+                <label>Email</label>
+                <input type="email"  placeholder="sebastian@email.com required"/>
+
+                <label>Email</label>
+                <input type="email"  placeholder="sebastian@email.com required"/>
+
+                <button type="submit">Acceder</button>
+            </form>
+        </div>
+    `
+}
